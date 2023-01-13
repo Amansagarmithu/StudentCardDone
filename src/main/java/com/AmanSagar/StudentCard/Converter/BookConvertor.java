c5ca5e1f4f8eceb67999b56a2da454d18d955b08
Aman Sagar <amansagar887@gmail.com>
2023-01-13 19:08:21 +0530

    bookDone

--- src/main/java/com/AmanSagar/StudentCard/Converter/BookConvertor.java
+++ src/main/java/com/AmanSagar/StudentCard/Converter/BookConvertor.java
@@ -4,8 +4,9 @@ import com.AmanSagar.StudentCard.RequestDTO.BookDto;
 import com.AmanSagar.StudentCard.modules.Author;
 import com.AmanSagar.StudentCard.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class BookConvertor {
     @Autowired
     AuthorRepository authorrepository;
